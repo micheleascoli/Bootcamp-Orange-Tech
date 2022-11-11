2119d2d9c924380f72f27c978619ab0d34c8ff54
micheleascoli <micheleascoli17@gmail.com>
2022-11-10 22:17:53 -0500

    Create lista-exercicio-objeto.js

---
+++ Primeiras-Paginas-Interativas-JavaScript/Criando-Objetos-e-Classes-em-JavaScript/lista-exercicio-objeto.js
@@ -0,0 +1,25 @@
+/**
+    1 - Crie uma classe pararepresentar carros.
+    Os carros possuem uma marca, uma cor e um gasto médio de combustível porl Km rodado.
+    Crie um método que dado a quantidade que quilômetros e o preço do combustível nos dê o 
+    valor gasto em reais para realizar este percurso.
+ */
+
+class Carro {
+    marca;
+    cor;
+    gastoMedioPorKm;
+
+    constructor(marca, cor, gastoMedioPorKm) {
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoMedioPorKm = gastoMedioPorKm;
+    }
+
+    calcularGastoDoPercurso(quantidadeKm, precoCombustivel){
+        return (quantidadeKm / this.gastoMedioPorKm) * precoCombustivel;
+    }
+}
+
+const c1 = new Carro('Nissan', 'Cinza', 10);
+console.log(c1.calcularGastoDoPercurso(70, 5));
