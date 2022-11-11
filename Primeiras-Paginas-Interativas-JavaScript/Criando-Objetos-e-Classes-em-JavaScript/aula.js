8ad7977bd566a5b8b95a78315acde12bad5f569b
micheleascoli <micheleascoli17@gmail.com>
2022-11-10 21:23:43 -0500

    objetos e classes

--- Primeiras-Paginas-Interativas-JavaScript/Criando-Objetos-e-Classes-em-JavaScript/aula.js
+++ Primeiras-Paginas-Interativas-JavaScript/Criando-Objetos-e-Classes-em-JavaScript/aula.js
@@ -1,15 +1,50 @@
-const pessoa = {
-    nome: 'Michele O Ascoli',
-    idade: 26,
 
-    descrever: function (){
-        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}.`);
+class Pessoa {
+    nome;
+    idade;
+    anoDeNascimento;
+
+    constructor (nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2022 - idade;
     }
-};
 
-pessoa.descrever = function(){
-    console.log(`Meu nome é ${this.nome}.`);
+    descrever() {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}. Nasci em ${this.anoDeNascimento}`);
+    }
 }
 
-pessoa.descrever();
+const p1 = new Pessoa('Michele O Ascoli', 26);
+// p1.nome = 'Michele O Ascoli';
+// p1.idade = 26;
 
+const p2 = new Pessoa('A A', 70);
+// p2.nome = 'A A';
+// p2.idade = 55
+
+p1.descrever();
+p2.descrever();
+
+// const pessoa1 = {
+//     nome: 'Michele O Ascoli',
+//     idade: 26,
+
+//     descrever: function (){
+//         console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}.`);
+//     }
+// };
+
+
+// pessoa.descrever = function(){
+//     console.log(`Meu nome é ${this.nome}.`);
+// }
+
+// pessoa.descrever();
+
+
+/** Outra forma de acessar os valores */
+// const atributo = 'idade';
+
+// anotação de coleção
+// console.log(pessoa[atributo]);
