cd8b39de8d5283761240fd6909d20b5a2ec6ff87
micheleascoli <micheleascoli17@gmail.com>
2022-11-14 17:57:20 -0500

    Aula importação e exportação com JavaScript

---
+++ Primeiras-Paginas-Interativas-JavaScript/Importacao-e-Exportacao-com-JavaScript/main.js
@@ -0,0 +1,34 @@
+/*
+    Uma sala contem 5 alunos e para cada aluno foi sorteado um numero de 1 - 100.
+    Faça um programa que receba os 5 números sorteados para os alunos e mostre o maior número sorteado.
+
+    Dados de entrada:
+    5
+    50
+    10
+    98
+    23
+
+    Saída:
+    98
+ */
+
+const { gets, print} = require('./funcoes-auxiliares');
+
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++) {
+    const numerosSorteado = gets();
+    numerosSorteados.push(numerosSorteado);
+}
+
+let numeroMaior = 0;
+
+for (let i = 0; i < numerosSorteados.length; i++) {
+    if(numerosSorteados[i] > numeroMaior){
+        numeroMaior = numerosSorteados[i];
+    }    
+}
+
+print(numeroMaior);
+
