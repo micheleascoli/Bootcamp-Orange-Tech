017b432bac1b1f76ce0047c19483939c058c68cf
micheleascoli <micheleascoli17@gmail.com>
2022-12-01 16:51:51 -0500

    manipulando o html

--- Primeiras-Paginas-Interativas-JavaScript/Pokedex/assets/js/main.js
+++ Primeiras-Paginas-Interativas-JavaScript/Pokedex/assets/js/main.js
@@ -1,2 +1,20 @@
 
-console.log(`Sucesso!`);
+function convertPokemonToLi(pokemon) {
+    return `            
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+               </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}" srcset="">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeAPI.getPokemons().then((pokemons = []) => pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join(''))
