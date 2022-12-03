3e0b7a7ce86a54d59168d5a9b080b23c214da3d6
micheleascoli <micheleascoli17@gmail.com>
2022-12-02 21:06:31 -0500

    inserindo as cores

--- Primeiras-Paginas-Interativas-JavaScript/Pokedex/assets/js/main.js
+++ Primeiras-Paginas-Interativas-JavaScript/Pokedex/assets/js/main.js
@@ -1,15 +1,17 @@
 
 function convertPokemonToLi(pokemon) {
     return `            
-        <li class="pokemon">
-            <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
+
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                     ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}" srcset="">
+                
+               <img src="${pokemon.photo}" 
+                    alt="${pokemon.name}">
             </div>
         </li>
     `
@@ -17,4 +19,6 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeAPI.getPokemons().then((pokemons = []) => pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join(''))
+pokeAPI.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join('')
+})
